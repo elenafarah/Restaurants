ed561f29b28a5178ef1fd0740c1e0b6ed0687e33
elenafarah <elenafarah@etu.univ-nantes.fr>
2025-09-05 21:16:57 +0200

    supprimer le projet IdentityHash

--- IdentityHashGen/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using Microsoft.AspNetCore.Identity;
-
-var password = args.Length > 0 ? args[0] : "P@ssw0rd!";
-var hasher = new PasswordHasher<IdentityUser>();
-var user = new IdentityUser("admin@test.com");
-
-var hash = hasher.HashPassword(user, password);
-Console.WriteLine(hash);
